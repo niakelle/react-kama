6cba900714fc858474761b9009442b1ad37a7d53
Niakelle <morak0398@gmail.com>
2022-08-12 23:02:35 +0300

    styling dialogs

--- 01-first-project/my-app/src/components/Dialogs/Dialogs.jsx
+++ 01-first-project/my-app/src/components/Dialogs/Dialogs.jsx
@@ -3,10 +3,19 @@ import styles from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
 	return (
-		<div>
-			Dialogs
-		</div>
-	);
+    <div className={styles.dialogs}>
+      <div className={styles.dialogsItems}>
+        <div className={styles.item + ' ' + styles.active}>Dmytry</div>
+        <div className={styles.item}>Sasha</div>
+        <div className={styles.item}>Valery</div>
+      </div>
+      <div className={styles.messages}>
+        <div className={styles.message}>Hi</div>
+        <div className={styles.message}>How are you?</div>
+        <div className={styles.message}>How are your holidays?</div>
+      </div>
+    </div>
+  );
 }
 
 export default Dialogs;
