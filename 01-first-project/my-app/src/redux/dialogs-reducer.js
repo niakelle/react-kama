7d5bee4ff367e19c9b4a745087f51fe8cd9405ab
niakelle <morak0398@gmail.com>
2022-09-02 04:54:10 +0300

    42 - added redux store instead of self-created store

--- 01-first-project/my-app/src/redux/dialogs-reducer.js
+++ 01-first-project/my-app/src/redux/dialogs-reducer.js
@@ -1,7 +1,22 @@
 const SEND_MESSAGE = "SEND-MESSAGE";
 const UPDATE_NEW_MESSAGE_TEXT = "UPDATE-NEW-MESSAGE-TEXT";
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+  dialogsData: [
+    { id: 1, name: "Dmytry" },
+    { id: 2, name: "Sasha" },
+    { id: 3, name: "Valery" },
+    { id: 4, name: "Sergey" },
+  ],
+  messagesData: [
+    { id: 1, message: "Hi" },
+    { id: 2, message: "How are you?" },
+    { id: 3, message: "How are your holidays?" },
+  ],
+  newMessageText: "",
+};
+
+const dialogsReducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_NEW_MESSAGE_TEXT:
       state.newMessageText = action.newMessageText;
--- 01-first-project/my-app/src/redux/navbar-reducer.js
+++ 01-first-project/my-app/src/redux/navbar-reducer.js
@@ -1,4 +1,12 @@
-const navbarReducer = (state, action) => {
+let initialState = {
+	friendsData: [
+		{ id: 1, name: "Dmytry" },
+		{ id: 2, name: "Sasha" },
+		{ id: 3, name: "Valery" },
+	],
+};
+
+const navbarReducer = (state = initialState, action) => {
 
 
 	return state;
