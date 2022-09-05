4edb2d672fef0f3caadfde888a56ee84e3c48fc9
niakelle <morak0398@gmail.com>
2022-09-06 00:57:04 +0300

    45 - added react-redux, used connects
    also played a bit with colouring of navbar. btw in this commit sending messages and adding posts don't work.

--- 01-first-project/my-app/src/components/Dialogs/DialogsContainer.jsx
+++ 01-first-project/my-app/src/components/Dialogs/DialogsContainer.jsx
@@ -1,39 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
 import {
   sendMessageCreator,
   updateNewMessageBodyCreator,
 } from "../../redux/dialogs-reducer";
-import StoreContext from "../../StoreContext";
 import Dialogs from "./Dialogs";
 
-const DialogsContainer = (props) => {
-  // receiving dialogsPage state from props and shortcutting it
-
-  // onClick function for sending messages. Uses imported action creator "sendMessageCreator"
-
-  return (
-    <StoreContext.Consumer>
-      {(store) => {
-        let dialogsPage = store.getState().dialogsPage;
-
-        let onSendMessageClick = () => {
-          store.dispatch(sendMessageCreator());
-        };
-
-        let onNewMessageTextChange = (body) => {
-          store.dispatch(updateNewMessageBodyCreator(body));
-        };
-
-        return (
-          <Dialogs
-            updateNewMessageBody={onNewMessageTextChange}
-            sendMessage={onSendMessageClick}
-            dialogsPage={dialogsPage}
-          />
-        );
-      }}
-    </StoreContext.Consumer>
-  );
+let mapStateToProps = (state) => {
+  return {
+    dialogsPage: state.dialogsPage,
+  };
 };
+
+let mapDispatchToProps = (dispatch) => {
+  return {
+    updateNewMessageBody: () => {
+      dispatch(sendMessageCreator());
+    },
+    sendMessage: (body) => {
+      dispatch(updateNewMessageBodyCreator(body));
+    },
+  };
+};
+
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs);
 
 export default DialogsContainer;
--- 01-first-project/my-app/src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ 01-first-project/my-app/src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,38 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
 import {
   addPostActionCreator,
   updateNewPostTextActionCreator,
 } from "../../../redux/profile-reducer";
-import StoreContext from "../../../StoreContext";
 import MyPosts from "./MyPosts";
 
-const MyPostsContainer = (props) => {
-  // let state = props.store.getState();
-
-  return (
-    <StoreContext.Consumer>
-      {(store) => {
-        let state = store.getState();
-        let addPost = () => {
-          store.dispatch(addPostActionCreator());
-        };
-
-        let onPostChange = (text) => {
-          let action = updateNewPostTextActionCreator(text);
-          store.dispatch(action);
-        };
-
-        return (
-          <MyPosts
-            updateNewPostText={onPostChange}
-            addPost={addPost}
-            postData={state.profilePage.postData}
-            newPostText={state.profilePage.newPostText}
-          />
-        );
-      }}
-    </StoreContext.Consumer>
-  );
+const mapStateToProps = (state) => {
+  return {
+    postData: state.profilePage.postData,
+    newPostText: state.profilePage.newPostText,
+  };
 };
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateNewPostText: (text) => {
+      dispatch(updateNewPostTextActionCreator(text));
+    },
+    addPost: () => {
+      dispatch(addPostActionCreator());
+    },
+  };
+};
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
 
 export default MyPostsContainer;
