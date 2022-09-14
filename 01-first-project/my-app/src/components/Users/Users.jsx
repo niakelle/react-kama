dd1939f2f593dffbafcdb0defbc763acd03667e6
niakelle <morak0398@gmail.com>
2022-09-14 13:07:32 +0300

    54 - moving get request to componentDidMount()

--- 01-first-project/my-app/src/components/Users/Users.jsx
+++ 01-first-project/my-app/src/components/Users/Users.jsx
@@ -4,75 +4,64 @@ import styles from "./Users.module.css";
 import userPhoto from "../../assets/profile-image.png";
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
-    if (this.props.users.length === 0) {
-      axios
+  componentDidMount() {
+    axios
         .get("https://jsonplaceholder.typicode.com/users")
         .then((response) => {
           this.props.setUsers(response.data);
         });
-    }
-  };
-
-  // getUsers = () => {
-  //   if (this.props.users.length === 0) {
-  //     axios
-  //       .get("https://jsonplaceholder.typicode.com/users")
-  //       .then((response) => {
-  //         this.props.setUsers(response.data);
-  //       });
-  //   }
-  // };
+  }
 
   render() {
-    return <div>
+    return (
       <div>
-        <button onClick={this.getUsers}>Get Users</button>
-      </div>
-      {this.props.users.map((u) => (
-        <div key={u.id}>
-          <span>
-            <div>
-              <img
-                src={u.picture != null ? u.picture : userPhoto}
-                alt=""
-                className={styles.img}
-              />
-            </div>
-            <div>
-              {u.followed ? (
-                <button
-                  onClick={() => {
-                    this.props.unfollow(u.id);
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    this.props.follow(u.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
-            <span>
-              <div>{u.name}</div>
-              <div>{u.email}</div>
-            </span>
-            <span>
-              <div>{u.address.city}</div>
-              <div>{u.address.street}</div>
-            </span>
-          </span>
+        <div>
+          <button onClick={this.getUsers}>Get Users</button>
         </div>
-      ))}
-    </div>;
+        {this.props.users.map((u) => (
+          <div key={u.id}>
+            <span>
+              <div>
+                <img
+                  src={u.picture != null ? u.picture : userPhoto}
+                  alt=""
+                  className={styles.img}
+                />
+              </div>
+              <div>
+                {u.followed ? (
+                  <button
+                    onClick={() => {
+                      this.props.unfollow(u.id);
+                    }}
+                  >
+                    Unfollow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(u.id);
+                    }}
+                  >
+                    Follow
+                  </button>
+                )}
+              </div>
+            </span>
+            <span>
+              <span>
+                <div>{u.name}</div>
+                <div>{u.email}</div>
+              </span>
+              <span>
+                <div>{u.address.city}</div>
+                <div>{u.address.street}</div>
+              </span>
+            </span>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
